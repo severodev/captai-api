d59c7c1392f2c3f212c7ab6bd06bfa2428458bda
annacmbrito <annet.rj@gmail.com>
2024-11-08 08:19:36 -0300

    Correção no filtro por data e valores de financiamento

--- src/edital/services/edital.service.ts
+++ src/edital/services/edital.service.ts
@@ -3,6 +3,8 @@ import { Between, FindManyOptions, FindOptionsWhere, In, LessThanOrEqual, Like, 
 import { Edital } from '../entity/edital.entity';
 import { PaginationMetadataDto } from 'src/util/interfaces/pagination-metadata.dto';
 import { EditalFilter } from '../interfaces/edital.filter';
+import moment from 'moment';
+import { Logger } from 'winston';
 
 @Injectable()
 export class EditalsService {
@@ -10,51 +12,65 @@ export class EditalsService {
     constructor(
         @Inject('EDITAIS_REPOSITORY')
         private editalRepository: Repository<Edital>,
+        @Inject('winston')
+        private readonly logger: Logger,
     ) { }
 
-    async findAll(filter: EditalFilter, pageOptions : PaginationMetadataDto): Promise<Edital[]> {
-        let query = this.editalRepository.createQueryBuilder('entity');
-    
-        if (filter.agencyList) {
-            let lista = filter.agencyList.split(',');
-            query = query.andWhere('entity.agency IN (:...lista)', { lista });
-        } else if (filter.agency) {
-            const searchValue = `%${filter.agency?.toUpperCase()}%`;
-            query = query.andWhere('entity.agency ILIKE :searchValue OR entity.title ILIKE :searchValue', { searchValue });
-        }
-    
-        if (filter.areaList) {
-            let lista = filter.areaList.split(',').map(item => item.trim());
-            query = query.andWhere('entity.areaList IN (:...lista)', { lista });
-        }
-    
-        if (filter.submission) {
-            let toDay = new Date();
-            toDay.setMinutes(0, 0, 0);
-            console.log('toDay ', new Date().setMinutes(0,0))
-            query = query.andWhere('entity.dt_submission <= :submission  AND entity.dt_submission >= :toDay', { submission: filter.submission, toDay: toDay });
-        }
-    
-        if (filter.financingValueHigh && filter.financingValueLow) {
-            query = query.andWhere('entity.nm_financing_value BETWEEN :low AND :high', {
-                low: filter.financingValueLow,
-                high: filter.financingValueHigh
-            });
-        }
-    
-        if (filter.maturity) {
-            query = query.andWhere('entity.maturityLevel LIKE :maturity', { maturity: `%${filter.maturity}%` });
-        }
+    async findAll(filter: EditalFilter, pageOptions: PaginationMetadataDto): Promise<Edital[]> {
 
-        if (filter.by && filter.order) {
-            query = query.orderBy(`entity.${filter.by}`, filter.order.toUpperCase() as 'ASC' | 'DESC');
+        try {
+
+            let query = this.editalRepository.createQueryBuilder('entity');
+
+            if (filter.agencyList) {
+                let lista = filter.agencyList.split(',');
+                query = query.andWhere('entity.agency IN (:...lista)', { lista });
+            } else if (filter.agency) {
+                const searchValue = `%${filter.agency?.toUpperCase()}%`;
+                query = query.andWhere('entity.agency ILIKE :searchValue OR entity.title ILIKE :searchValue', { searchValue });
+            }
+
+            if (filter.areaList) {
+                let lista = filter.areaList.split(',').map(item => item.trim());
+                query = query.andWhere('entity.areaList IN (:...lista)', { lista });
+            }
+
+            if (filter.submission) {
+                let startDate = filter.submission;
+                let endDate = new Date().setHours(24, 59, 59).toString();
+                // let toDay = new Date();
+                // toDay.setHours(24, 59, 59);
+                // console.log('toDay ', new Date().setMinutes(0,0))
+                query = query.andWhere('entity.dt_submission >= :startDate', { startDate });
+            }
+
+            if (filter.financingValueHigh && filter.financingValueLow) {
+                query = query.andWhere('(entity.nm_financing_value IS NULL OR entity.nm_financing_value BETWEEN :low AND :high)', {
+                    low: filter.financingValueLow,
+                    high: filter.financingValueHigh
+                });
+            }
+
+            if (filter.maturity) {
+                query = query.andWhere('entity.maturityLevel LIKE :maturity', { maturity: `%${filter.maturity}%` });
+            }
+
+            if (filter.by && filter.order) {
+                query = query.orderBy(`entity.${filter.by}`, filter.order.toUpperCase() as 'ASC' | 'DESC');
+            }
+
+            if (pageOptions.itemsPerPage) {
+                query = query.take(pageOptions.itemsPerPage);
+            }
+
+            return query.getMany();
+        } catch (err) {
+            // console.error(error);
+            this.logger.error(
+                'Problema na consulta de editais',
+                err,
+              );
         }
-    
-        if (pageOptions.itemsPerPage) {
-            query = query.take(pageOptions.itemsPerPage);
-        }
-    
-        return query.getMany();
     }
 
     async findOne(id: number): Promise<Edital> {
